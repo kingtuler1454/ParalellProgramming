e3186a95f9626962dae01d29771d25580011b00c
kingtuler <morozovsergey.03@gmail.com>
2024-02-24 02:06:53 +0400

    add data

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@
 using namespace std;
 
 
-std::vector<std::vector<int>> ReadMatrix(string name) {
-    std::vector<std::vector<int>> matrix;
+vector<vector<int>> ReadMatrix(string name) {
+    vector<vector<int>> matrix;
     ifstream myfile;
     myfile.open(name);
     if (myfile.is_open()) {
@@ -17,52 +17,52 @@ std::vector<std::vector<int>> ReadMatrix(string name) {
         string line;
         while (getline(myfile, line))
         {
-            std::vector<int> tmp;
+            vector<int> tmp;
 
-            std::istringstream iss(line); // Создаем строковый поток для разбора текущей строки
+            istringstream iss(line); // Создаем строковый поток для разбора текущей строки
 
-            int num; 
+            int num;
             while (iss >> num) { // Считываем числа из строки
-                tmp.push_back(num); 
+                tmp.push_back(num);
             }
             matrix.push_back(tmp);
         }
         myfile.close();
-        cout << "Succsesfull"<< endl;
+        cout << "Succsesfull" << endl;
     }
     else {
-        cout << "We can't opened file '"<<name<<"' , enter any button to next " << endl; 
+        cout << "We can't opened file '" << name << "' , enter any button to next " << endl;
         getchar();
     }
     return matrix;
 }
 
 
-void PrintMatrix(std::vector<std::vector<int>> matrix,int tmp) {
-    cout << "Matrix"<<tmp<<": " << endl;
+void PrintMatrix(vector<vector<int>> matrix, int tmp) {
+    cout << "Matrix" << tmp << ": " << endl;
     for (const auto& row : matrix) {
         for (int num : row) {
-            std::cout << num << " ";
+            cout << num << " ";
         }
-        std::cout << std::endl;
+        cout << endl;
     }
 }
 
 
-std::vector<std::vector<int>> Multiplication(std::vector<std::vector<int>>MatrixOne, std::vector<std::vector<int>>MatrixTwo)
+vector<vector<int>> Multiplication(vector<vector<int>>MatrixOne, vector<vector<int>>MatrixTwo)
 {
-    std::vector<std::vector<int>> main_tmp;
-    if (MatrixOne[0].size() !=MatrixTwo.size()) {
+    vector<vector<int>> main_tmp;
+    if (MatrixOne[0].size() != MatrixTwo.size()) {
         cout << "Sizes of matrices are not compatible for multiplication" << endl;
         return main_tmp; // Returning an empty matrix as multiplication is not possible
     }
 
-    for (int i = 0; i <MatrixOne.size(); i++) {
-        std::vector<int> supporting_tmp;
-        for (int j = 0; j <MatrixTwo[0].size(); j++) {
+    for (int i = 0; i < MatrixOne.size(); i++) {
+        vector<int> supporting_tmp;
+        for (int j = 0; j < MatrixTwo[0].size(); j++) {
             int sum = 0;
-            for (int k = 0; k <MatrixTwo.size(); k++) {
-                sum +=MatrixOne[i][k] *MatrixTwo[k][j];
+            for (int k = 0; k < MatrixTwo.size(); k++) {
+                sum += MatrixOne[i][k] * MatrixTwo[k][j];
             }
             supporting_tmp.push_back(sum);
         }
@@ -71,10 +71,14 @@ std::vector<std::vector<int>> Multiplication(std::vector<std::vector<int>>Matrix
     return main_tmp;
 }
 
-bool Verification(std::vector<std::vector<int>> CppMatrix) {
+bool Verification(vector<vector<int>> CppMatrix, string path1,string path2) {
 
-    system("python python_check.py");
-    std::vector<std::vector<int>> PythonMatrix = ReadMatrix("matrix_result_python.txt");
+    
+    string command = "python python_check.py " + path1 + " " + path2;
+    system(command.c_str());
+    vector<vector<int>> PythonMatrix = ReadMatrix("data\\matrix_result_python.txt");
+    
+
     for (int i = 0; i < CppMatrix.size(); i++) {
         for (int j = 0; j < CppMatrix[0].size(); j++) {
             if (PythonMatrix[i][j] != CppMatrix[i][j])
@@ -84,11 +88,11 @@ bool Verification(std::vector<std::vector<int>> CppMatrix) {
             }
         }
     }
-    cout << "Multiplication of matrix is True";
+    cout << "Multiplication of matrix is True"<<endl;
     return true;
 }
-void WriteMatrix(std::vector<std::vector<int>> matrix) {
-    std::ofstream outFile("matrix3.txt");
+void WriteMatrix(vector<vector<int>> matrix) {
+    ofstream outFile("data\\matrix3.txt");
     if (outFile.is_open()) {
         for (const auto& row : matrix) {
             for (int value : row) {
@@ -102,36 +106,32 @@ void WriteMatrix(std::vector<std::vector<int>> matrix) {
     else {
         cout << "Unable to open file matrix3 for writing." << endl;
     }
-
-
-
 }
 
 
 
-void cycle() {
-
-    std::vector<std::vector<int>> MatrixOne, MatrixTwo, MatrixCppFinal;
-   MatrixOne = ReadMatrix("matrix1.txt");
-   MatrixTwo = ReadMatrix("matrix2.txt");
+void cycle(string path1,string path2) {
+    cout << "___________________Work with files: " << path1<< " and "<<path2 <<"___________________" << endl;
+    vector<vector<int>> MatrixOne, MatrixTwo, MatrixCppFinal;
+    MatrixOne = ReadMatrix(path1);
+    MatrixTwo = ReadMatrix(path2);
     PrintMatrix(MatrixOne, 1);
     PrintMatrix(MatrixTwo, 2);
-    auto start_time = std::chrono::high_resolution_clock::now();
-    MatrixCppFinal = Multiplication(MatrixOne,MatrixTwo);
-    auto end_time = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
-    std::cout << "Time of work: " << duration.count() << " microseconds" << std::endl;
-    std::cout << "Elements in  final matrix: " <<MatrixCppFinal.size() * MatrixCppFinal[0].size() << std::endl;
+    auto start_time = chrono::high_resolution_clock::now();
+    MatrixCppFinal = Multiplication(MatrixOne, MatrixTwo);
+    auto end_time = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
+    cout << "Time of work: " << duration.count() << " microseconds" << endl;
+    cout << "Elements in  final matrix: " << MatrixCppFinal.size() * MatrixCppFinal[0].size() << endl;
     PrintMatrix(MatrixCppFinal, 3);
     WriteMatrix(MatrixCppFinal);
-    Verification(MatrixCppFinal);
-
-
-
+    Verification(MatrixCppFinal, path1, path2);
 }
 
 int main() {
-
+    cycle("data\\A1.txt","data\\B1.txt");
+    cycle("data\\A2.txt", "data\\B2.txt");
+    cycle("data\\A3.txt", "data\\B3.txt");
     return 0;
 }
 
